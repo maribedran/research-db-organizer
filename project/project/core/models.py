842e563133f25a2fd424ea9544b407712b8061a6
Mari <maribedran@gmail.com>
2015-04-03 21:41:49 -0300

    Add null and blank options to fields and unicode to models

--- project/project/core/models.py
+++ project/project/core/models.py
@@ -4,26 +4,26 @@ from django.db import models
 class Publicacao(models.Model):
     titulo = models.CharField(max_length=200)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.titulo
 
 
 class Referencia(models.Model):
     titulo = models.CharField(max_length=200)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.titulo
 
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
-    nascimento = models.DateField()
-    morte = models.DateField()
-    nacionalidade = models.CharField(max_length=30)
-    formacao = models.CharField(max_length=100)
-    ocupacao = models.CharField(max_length=100)
+    nascimento = models.DateField(null=True, blank=True)
+    morte = models.DateField(null=True, blank=True)
+    nacionalidade = models.CharField(max_length=30, null=True, blank=True)
+    formacao = models.CharField(max_length=100, null=True, blank=True)
+    ocupacao = models.CharField(max_length=100, null=True, blank=True)
 
-    observacoes = models.CharField(max_length=1000)
+    observacoes = models.CharField(max_length=1000, null=True, blank=True)
 
     #Publicacao
     publicacoes = models.ManyToManyField(to=Publicacao,
@@ -42,7 +42,7 @@ class Pessoa(models.Model):
     # empresas relacionadas
     # tipo ???
 
-    def __str__(self):
+    def __unicode__(self):
         return self.nome
 
     class Meta:
@@ -51,17 +51,17 @@ class Pessoa(models.Model):
 
 class Entidade(models.Model):
     nome = models.CharField(max_length=200)
-    fundacao = models.DateField()
-    encerramento = models.DateField()
-    fundador = models.CharField(max_length=200)
-    objetivos = models.CharField(max_length=200)
-    carater = models.CharField(max_length=200)
-    financiamento = models.CharField(max_length=100)
-    acoes = models.CharField(max_length=200)
+    fundacao = models.DateField(null=True, blank=True)
+    encerramento = models.DateField(null=True, blank=True)
+    fundador = models.CharField(max_length=200, null=True, blank=True)
+    objetivos = models.CharField(max_length=200, null=True, blank=True)
+    carater = models.CharField(max_length=200, null=True, blank=True)
+    financiamento = models.CharField(max_length=100, null=True, blank=True)
+    acoes = models.CharField(max_length=200, null=True, blank=True)
 
-    grupos_representados = models.CharField(max_length=200)
+    grupos_representados = models.CharField(max_length=200, null=True, blank=True)
 
-    observacoes = models.CharField(max_length=1000)
+    observacoes = models.CharField(max_length=1000, null=True, blank=True)
 
     #Publicacao
     publicacoes = models.ManyToManyField(to=Publicacao,
@@ -79,7 +79,7 @@ class Entidade(models.Model):
     diretores = models.ManyToManyField(to=Pessoa,
                                   related_name="cargo_de_direcao")
 
-    def __str__(self):
+    def __unicode__(self):
         return self.nome
 
     class Meta:
