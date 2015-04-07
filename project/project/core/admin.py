9f82fc2ef53db79d691bcefd2bed9e69bb746ac6
Mariana Bedran Lesche <maribedran@gmail.com>
2015-04-07 00:06:34 -0300

    Deixa apenas modelos editáveis no admin

--- project/project/core/admin.py
+++ project/project/core/admin.py
@@ -1,48 +1,44 @@
 from django.contrib import admin
 from models import (Publicacao,
                     Referencia,
-                    Pessoa,
-                    Entidade,
+                    MoradorDeFavela,
+                    Empresario,
                     EntidadeEmpresarial,
                     Estatal,
                     AssociacaoDeFavela)
 
-
+@admin.register(Publicacao)
 class PublicacaoAdmin(admin.ModelAdmin):
     list_filter = ('titulo',)
 
 
+@admin.register(Referencia)
 class ReferenciaAdmin(admin.ModelAdmin):
     list_filter = ('titulo',)
 
 
-class PessoaAdmin(admin.ModelAdmin):
-    list_filter = ('nome', 'publicacoes', 'referencias')
-    list_display = ('nome',)
-    list_select_related = ('publicacoes', 'referencias', 'cargo_de_direcao', 'cargo_publico',
-    'entidade_empresarial')
-
-
-class EntidadeAdmin(admin.ModelAdmin):
+@admin.register(MoradorDeFavela)
+class MoradorDeFavelaAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Empresario)
+class EmpresarioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EntidadeEmpresarial)
 class EntidadeEmpresarialAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Estatal)
 class EstatalAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(AssociacaoDeFavela)
 class AssociacaoDeFavelaAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Publicacao, PublicacaoAdmin)
-admin.site.register(Referencia, ReferenciaAdmin)
-admin.site.register(Pessoa, PessoaAdmin)
-admin.site.register(Entidade, EntidadeAdmin)
-admin.site.register(EntidadeEmpresarial, EntidadeEmpresarialAdmin)
-admin.site.register(Estatal, EstatalAdmin)
-admin.site.register(AssociacaoDeFavela, AssociacaoDeFavelaAdmin)
