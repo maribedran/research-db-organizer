66967b1e0726067f32e9f61833bb517d7fcaaa6a
Mariana Bedran Lesche <maribedran@gmail.com>
2015-04-07 16:39:34 +0100

    Adiciona inlines ao admin

--- project/project/core/admin.py
+++ project/project/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from models import (Publicacao,
                     Referencia,
+                    Pessoa,
                     MoradorDeFavela,
                     Empresario,
                     EntidadeEmpresarial,
@@ -17,28 +18,54 @@ class ReferenciaAdmin(admin.ModelAdmin):
     list_filter = ('titulo',)
 
 
+class FuncionalismoPublicoInline(admin.TabularInline):
+    model = Pessoa.cargo_publico.through
+
+
+class MembroDeAssociacaoInline(admin.TabularInline):
+    model = MoradorDeFavela.associacao_de_favela.through
+
+
+class MembroDeEntidadeInline(admin.TabularInline):
+    model = Empresario.entidade_empresarial.through
+
+
 @admin.register(MoradorDeFavela)
 class MoradorDeFavelaAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        FuncionalismoPublicoInline,
+        MembroDeAssociacaoInline,
+    ]
+    exclude = ('cargo_publico', 'associacao_de_favela')
 
 
 @admin.register(Empresario)
 class EmpresarioAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        FuncionalismoPublicoInline,
+        MembroDeEntidadeInline,
+    ]
+    exclude = ('cargo_publico', 'entidade_empresarial')
 
 
 @admin.register(EntidadeEmpresarial)
 class EntidadeEmpresarialAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        MembroDeEntidadeInline,
+    ]
 
 
 @admin.register(Estatal)
 class EstatalAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        FuncionalismoPublicoInline,
+    ]
 
 
 @admin.register(AssociacaoDeFavela)
 class AssociacaoDeFavelaAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        MembroDeAssociacaoInline,
+    ]
 
 
