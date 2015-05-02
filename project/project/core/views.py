a48c0cc6ea7f1843757206cf5b2d68b587aec0e5
Mariana Bedran Lesche <maribedran@gmail.com>
2015-05-02 18:51:10 -0300

    View renderizando todos os objetos

--- project/project/core/views.py
+++ project/project/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from project.core.models import MoradorDeFavela, Empresario, Burocrata, AssociacaoDeFavela, EntidadeEmpresarial, Estatal
+
+class HomeView(TemplateView):
+
+    template_name = "core/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context['favelados'] = MoradorDeFavela.objects.all()
+        context['empresarios'] = Empresario.objects.all()
+        context['burocratas'] = Burocrata.objects.all()
+        context['associacoes'] = AssociacaoDeFavela.objects.all()
+        context['entidades'] = EntidadeEmpresarial.objects.all()
+        context['estatais'] = Estatal.objects.all()
+
+        return context
