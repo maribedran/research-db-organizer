133e73ec878d623fd510022fb43a5d92735609dc
Mariana Bedran Lesche <maribedran@gmail.com>
2015-05-02 18:52:13 -0300

    Urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from project.core.views import HomeView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
